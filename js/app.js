203ad725fb5fe406394a7ee76050e31bbd7db43f
mariasolegallina <gallinamariasole@gmail.com>
2023-10-25 13:30:12 +0200

    add - files + vue

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const {createApp} = Vue
+
+createApp({
+    data() {
+        return {
+
+        }
+    },
+    methods() {
+
+    },
+    mounted() {
+        console.log("OK");
+    }
+
+}).mount('#app')
